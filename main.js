5434d2b9dd0e5999f6d45d38abc843f89a44bc49
Luke Sorvik <lukesorvik@yahoo.com>
2024-03-22 17:24:55 -0700

    not showing anything

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css' //import the css file
+import * as THREE from 'three' //import the three.js library
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene(); // Create a scene
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); // Create a camera
+//(fov, aspect ratio based off users browser window, cutoff for how close objects are visible near, far)
 
-setupCounter(document.querySelector('#counter'))
+// Create a renderer, render the canvas with the id of 'bg'
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio); //set pixel ratio to device pixel ratio
+renderer.setSize(window.innerWidth, window.innerHeight); //set size of renderer to window size, full screen canvas
+camera.position.setZ(30); //set camera position to 30 units back
+
+renderer.render(scene, camera); //render the scene with the camera
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100); //create a torus geometry
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347, wireframe : true }); //create a material with a color
+const torus = new THREE.Mesh(geometry, material); //create a mesh with the geometry and material
+
+scene.add(torus); //add the torus to the scene
+
+//we want to set up a recursive function to set up infinite loop to animate automatically
+function animate() {
+  requestAnimationFrame(animate); //request animation frame to animate
+  renderer.render(scene, camera); //render the scene with the camera
+}
+
+animate(); //call the animate function
